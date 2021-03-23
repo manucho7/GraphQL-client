53bd035873a9bf7eba744d0c221c526e779331ec
manucho <manuel.blanco77@gmail.com>
2021-03-23 11:13:21 -0300

    Removing errors and importing Nav inside App component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Nav = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand">Navbar</a>
+            <Link className="navbar-brand" to="/">Navbar</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
@@ -11,27 +12,15 @@ const Nav = () => {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
-                        <a className="nav-link" >Home <span class="sr-only">(current)</span></a>
+                        <Link className="nav-link" to="/login">
+                            Login
+                        </Link>
                     </li>
 
                     <li className="nav-item">
-                        <a className="nav-link" >Link</a>
-                    </li>
-
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a className="dropdown-item">Action</a>
-                            <a className="dropdown-item">Another action</a>
-                            <div className="dropdown-divider"></div>
-                            <a className="dropdown-item">Something else here</a>
-                        </div>
-                    </li>
-
-                    <li className="nav-item">
-                        <a className="nav-link disabled">Disabled</a>
+                        <Link className="nav-link" to="/register">
+                            Register
+                        </Link>
                     </li>
                 </ul>
 
