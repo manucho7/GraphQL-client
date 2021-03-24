30fb4e6d8c75b04fda3fe2aadff839717c432662
manucho <manuel.blanco77@gmail.com>
2021-03-24 09:09:59 -0300

    react router + Login component + Register component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ApolloClient from 'apollo-boost';
+import { Switch, Route } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
 //Import components
-import Home from './pages/Home';
 import Nav from './components/Nav';
+import Home from './pages/Home';
+import Register from './pages/auth/Register';
+import Login from './pages/auth/Login';
 
 const client = new ApolloClient({
     uri: process.env.REACT_APP_GRAPHQL_ENDPOINT
@@ -12,8 +15,12 @@ const client = new ApolloClient({
 const App = () => {
     return (
         <ApolloProvider client={client}>
-            <Nav />
-            <Home />
+        <Nav />
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/register" component={Register}/>
+                <Route exact path="/login" component={Login}/>
+            </Switch>
         </ApolloProvider>
     );
 }
---
+++ src/pages/auth/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div className="container p-5">
+            <div className="row p-5">
+                <h4>Login</h4>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
