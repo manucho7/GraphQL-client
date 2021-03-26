b305cb96848cea0d99569acb4fd30ad802208090
manucho <manuel.blanco77@gmail.com>
2021-03-26 16:32:28 -0300

    Login + Register components

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const Register = () => {
+
+    const [email, setEmail] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = () => {
+        
+    }
+
     return (
         <div className="container p-5">
-            <div className="row p-5">
-                <h4>Register</h4>
-            </div>
+            <h4>Register</h4>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Email Address</label>
+
+                    <input className="form-control" 
+                        type="email" 
+                        value={email} 
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Enter email"
+                        disabled={loading}
+                    />
+
+                </div>
+                <button className="btn btn-raised btn-primary" disabled={!email || loading}>Submit</button>
+            </form>
         </div>
     )
 }
